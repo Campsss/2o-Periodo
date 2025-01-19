e462591eb94f02623d0b0a561378dc6977d99e76
Campsss <161540562+Campsss@users.noreply.github.com>
2025-01-19 14:28:37 -0300

    Delete ninja directory

--- ninja/src/uchiha.java
+++
@@ -1,9 +0,0 @@
-public class uchiha extends ninja {
-
-    String nivelDoSharingan = "Sharingan 2";
-    
-    public void sharinganAtivado(){
-
-        System.out.println("Sharingan Ativado");
-    }
-}
--- ninja/src/uzumaki.java
+++
@@ -1,8 +0,0 @@
-public class uzumaki extends ninja {
-
-    boolean temBijuu;
-    
-    public void chakraInfinito(){
-        System.out.println("Voce é um uzumaki entao vc tem chakra infinito");
-    }
-}
