45823c10e93ecd7b72639741765819b7d2b27874
Campsss <161540562+Campsss@users.noreply.github.com>
2025-01-19 14:27:15 -0300

    Add files via upload

---
+++ ninja/src/uchiha.java
@@ -0,0 +1,9 @@
+public class uchiha extends ninja {
+
+    String nivelDoSharingan = "Sharingan 2";
+    
+    public void sharinganAtivado(){
+
+        System.out.println("Sharingan Ativado");
+    }
+}
